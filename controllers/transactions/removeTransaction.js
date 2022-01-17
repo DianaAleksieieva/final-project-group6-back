4e86426d47cfbc8ca037641f727973136d6c9445
dimapashchenko1 <76619935+dimapashchenko1@users.noreply.github.com>
2022-01-17 20:07:08 +0200

    remove transaction

--- controllers/transactions/removeTransaction.js
+++ controllers/transactions/removeTransaction.js
@@ -1,5 +1,30 @@
+import { Transaction } from '../../schemas/mongoose/index.js';
+import { User } from '../../schemas/mongoose/index.js';
+
 const removeTransaction = async (req, res) => {
-  console.log('req', req);
-  console.log('res', res);
-};
+
+const userId = req.user.id;
+const transactionId = req.params.id
+const currentBalance = req.user.currentBalance;
+console.log('id_user', userId);
+console.log('id_transaction', transactionId);
+console.log('currentBalance', currentBalance);
+
+const transaction = await Transaction.findOneAndRemove({
+  _id: transactionId,
+  owner: userId,
+});
+
+const { amount } =transaction; 
+console.log("amount", amount);
+const newBalance = currentBalance + amount;
+console.log("newBalance", newBalance);
+console.log(`Transaction with id=${transactionId} removed`);
+
+
+// await User.findByIdAndUpdate(
+//   { id,
+//     currentBalance: newBalance },
+// );
+}
 export default removeTransaction;
