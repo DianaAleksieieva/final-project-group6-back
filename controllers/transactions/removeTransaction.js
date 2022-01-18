77696fe2541b5a9030eb348efb50abb6ef5e9c7a
dimapashchenko1 <76619935+dimapashchenko1@users.noreply.github.com>
2022-01-18 16:33:32 +0200

    remove transactions done

--- controllers/transactions/removeTransaction.js
+++ controllers/transactions/removeTransaction.js
@@ -2,13 +2,9 @@ import { Transaction } from '../../schemas/mongoose/index.js';
 import { User } from '../../schemas/mongoose/index.js';
 
 const removeTransaction = async (req, res) => {
-
 const userId = req.user.id;
 const transactionId = req.params.id
 const currentBalance = req.user.currentBalance;
-console.log('id_user', userId);
-console.log('id_transaction', transactionId);
-console.log('currentBalance', currentBalance);
 
 const transaction = await Transaction.findOneAndRemove({
   _id: transactionId,
@@ -16,15 +12,17 @@ const transaction = await Transaction.findOneAndRemove({
 });
 
 const { amount } =transaction; 
-console.log("amount", amount);
-const newBalance = currentBalance + amount;
-console.log("newBalance", newBalance);
-console.log(`Transaction with id=${transactionId} removed`);
+const newBalance = Number(currentBalance) + Number(amount);
+
+const user = await User.findByIdAndUpdate(
+  {
+    _id: userId},
+    {currentBalance: newBalance.toString(),
+  }
+);
 
 
-// await User.findByIdAndUpdate(
-//   { id,
-//     currentBalance: newBalance },
-// );
+  res.status(201).json({status: 'success', _id: userId, currentBalance: newBalance.toString()});
+
 }
 export default removeTransaction;
