594ae0b41db8b712db3df92fcb31620d80c51dae
sepaev <sepaev@gmail.com>
2022-01-20 18:18:00 +0200

    fix

--- controllers/transactions/index.js
+++ controllers/transactions/index.js
@@ -4,7 +4,7 @@ import removeTransactionController from './removeTransactionController.js';
 import removeTransaction from './removeTransaction.js';
 import getYearlyByTypeController from './getYearlyByTypeController.js';
 import getMonthlyByTypeController from './getMonthlyByTypeController.js';
-import getAllMonthlyByTypeTransactions from './getMonthlyByTypeController.js';
+import getAllMonthlyByCategoryController from './getAllMonthlyByCategoryController.js';
 import putSetOfTransactionsController from './putSetOfTransactionsController.js';
 
 export default {
@@ -13,7 +13,7 @@ export default {
   removeTransactionController,
   removeTransaction,
   getYearlyByTypeController,
+  getAllMonthlyByCategoryController,
   getMonthlyByTypeController,
-  getAllMonthlyByTypeTransactions,
   putSetOfTransactionsController,
 };
--- models/transactions/getAllMonthlyByCategoryModel.js
+++ models/transactions/getAllMonthlyByCategoryModel.js
@@ -1,10 +1,10 @@
 import { Transaction } from '../../schemas/mongoose/index.js';
 
 const getAllMonthlyByCategoryModel = async (month, year, category, user) => {
+  console.log(month, year, category, user);
   return await Transaction.find(
     { month, year, category, owner: user._id },
     '_id date amount type category description date',
-
   );
 };
 export default getAllMonthlyByCategoryModel;
